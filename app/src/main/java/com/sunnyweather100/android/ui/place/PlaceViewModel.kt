320bbea0b71611c47cf09e58af77d78611e9a680
wangmouren <2046502751@qq.com>
2021-01-10 14:01:00 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyweather100/android/ui/place/PlaceViewModel.kt
@@ -0,0 +1,24 @@
+package com.sunnyweather100.android.ui.place
+
+import androidx.lifecycle.*
+
+import com.sunnyweather100.android.logic.Repository
+import com.sunnyweather100.android.logic.model.Place
+
+class PlaceViewModel : ViewModel() {
+
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = Transformations.switchMap(searchLiveData) { query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query: String) {
+        searchLiveData.value = query
+    }
+
+
+
+}
